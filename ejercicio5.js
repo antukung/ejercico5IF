d22d2b779f848e4f49285cde8259684cf586b94e
Antu <122485403+antukung@users.noreply.github.com>
2023-04-18 09:57:01 -0300

    segunda parte del ejercicio
    aca estamos haciendo el ejercico y rompiendonoos los ccocos

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,7 +1,9 @@
 let mascotaIngresada = prompt ("ingrese la tantidad de mascotas");
+let tiermpoPaseoIngresado = prompt ("tiempo del paseo en minutos")
 
-if (mascotaIngresada <= 3) {
-    document.write ("es posible realizar el paseo")
+if ((mascotaIngresada <= 3) && ((tiermpoPaseoIngresado>= 30) && (tiermpoPaseoIngresado<=60))) {
+    document.write ("<h4>es posible realizar el paseo</h4>")
+    document.write ("<h4>el paseo durara </h4>"+tiermpoPaseoIngresado)
     
 } else {
     alert ("no es posible realizar el paseo")
