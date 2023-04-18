185c30f0978c284932b819fcfecccbdaf2524783
Antu <122485403+antukung@users.noreply.github.com>
2023-04-18 09:48:31 -0300

    parte 1 del ejercicio
    aca esta la primer parte creo

---
+++ ejercicio5.js
@@ -0,0 +1,9 @@
+let mascotaIngresada = prompt ("ingrese la tantidad de mascotas");
+
+if (mascotaIngresada <= 3) {
+    document.write ("es posible realizar el paseo")
+    
+} else {
+    alert ("no es posible realizar el paseo")
+}
+
